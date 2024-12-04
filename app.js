0626c380e4b28aa22b580e4c5569c5e5bb468e71
Alex Albu <alex.albu@tss-yonder.com>
2024-12-04 13:17:24 +0200

    New websocket server setup

--- app.js
+++ app.js
@@ -1,61 +1,70 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const http = require("http");
+const { Server } = require("socket.io");
 
-app.get("/", (req, res) => res.type('html').send(html));
+const lobbies = {};
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const httpServer = http.createServer();
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+io.on("connection", (socket) => {
+  console.log("Player connected:", socket.id);
+
+  socket.on("createLobby", ({ playerName, words }) => {
+    const lobbyId = generateId();
+    const playerId = socket.id;
+
+    socket.lobbyId = lobbyId;
+
+    lobbies[lobbyId] = {
+      id: lobbyId,
+      players: {
+        [playerId]: {
+          name: playerName,
+          words,
+          usedWords: [],
+        },
+      },
+      playerOrder: [playerId],
+      gameStarted: false,
+      currentTurnIndex: 0,
+    };
+
+    socket.join(lobbyId);
+    socket.emit("lobbyCreated", { lobbyId, playerId });
+    console.log(`Lobby ${lobbyId} created by player ${playerName}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Player disconnected:", socket.id);
+    leaveLobby(socket);
+  });
+});
+
+function generateId() {
+  return Math.random().toString(36).substring(2, 9);
+}
+
+function leaveLobby(socket) {
+  const lobby = lobbies[socket.lobbyId];
+  if (lobby) {
+    delete lobby.players[socket.id];
+    lobby.playerOrder = lobby.playerOrder.filter((id) => id !== socket.id);
+
+    io.to(socket.lobbyId).emit("playerLeft", { playerId: socket.id });
+
+    if (Object.keys(lobby.players).length === 0) {
+      delete lobbies[socket.lobbyId];
+      console.log(`Lobby ${socket.lobbyId} deleted.`);
+    }
+  }
+}
+
+httpServer.listen(process.env.PORT || 3000, "0.0.0.0", () => {
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
+});
